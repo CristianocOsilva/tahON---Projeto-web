a67c52241d6b6b043328645f11ed2852be5785a5
CristianocOsilva <cristianocezar.logistica@gmail.com>
2022-06-02 23:32:15 -0300

    alteração do templates e app.py

--- app.py
+++ app.py
@@ -277,9 +277,5 @@ def unauthorized():
 def create_tables():
     db.create_all()
 
-    
-
-
-
 if __name__ == '__main__':
-   app.run(debug = True, port=5000)
+   app.run(debug=True, host='localhost')
